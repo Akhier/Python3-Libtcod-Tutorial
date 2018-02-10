57166072b6aaf9edb23d454b21118fffa57e26af
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-02-10 00:26:00 -0500

    change to match Pep8 guidelines

--- engine.py
+++ engine.py
@@ -17,7 +17,8 @@ def main():
 
     while not libtcod.console_is_window_closed():
         libtcod.console_set_default_foreground(0, libtcod.white)
-        libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(
+            0, player_x, player_y, '@', libtcod.BKGND_NONE)
         libtcod.console_flush()
 
         key = libtcod.console_check_for_keypress()
