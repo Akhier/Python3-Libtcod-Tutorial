655822f6056d979e3e55ea86b980049ee67d8e97
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-02-10 00:22:46 -0500

    change to match Pep8 guidelines

--- engine.py
+++ engine.py
@@ -5,9 +5,12 @@ def main():
     screen_width = 80
     screen_height = 50
 
-    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    libtcod.console_set_custom_font(
+        'arial10x10.png',
+        libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    libtcod.console_init_root(
+        screen_width, screen_height, 'libtcod tutorial revised', False)
 
     while not libtcod.console_is_window_closed():
         libtcod.console_set_default_foreground(0, libtcod.white)
