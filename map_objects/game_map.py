5cd3304b4b8698398f21012dda327b3b5e0b67dd
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-02-13 00:13:28 -0500

    Missed the y for loop in create_room method

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -17,8 +17,9 @@ class GameMap:
     def create_room(self, room):
         # go through the tiles in the rectangle and make them passable
         for x in range(room.x1 + 1, room.x2):
-            self.tiles[x][y].blocked = False
-            self.tiles[x][y].block_sight = False
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
