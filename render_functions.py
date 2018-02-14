8500de9fc0733c070de388e98c72671fd3e263d4
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-02-14 00:47:56 -0500

    Change draw_entity to only show visible entities

--- render_functions.py
+++ render_functions.py
@@ -41,10 +41,11 @@ def clear_all(con, entities):
         clear_entity(con, entity)
 
 
-def draw_entity(con, entity):
-    libtcod.console_set_default_foreground(con, entity.color)
-    libtcod.console_put_char(
-        con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+def draw_entity(con, entity, fov_map):
+    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        libtcod.console_set_default_foreground(con, entity.color)
+        libtcod.console_put_char(
+            con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
 
 
 def clear_entity(con, entity):
