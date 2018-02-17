27ddfd79cf143392e6a972f04bbcc4a54565d00f
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-02-17 00:10:18 -0500

    Add ability to display messages with render_all

--- render_functions.py
+++ render_functions.py
@@ -29,7 +29,7 @@ def render_bar(panel, x, y, total_width, name, value,
 
 
 def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute,
-               screen_width, screen_height, bar_width,
+               message_log, screen_width, screen_height, bar_width,
                panel_height, panel_y, colors):
     if fov_recompute:
         # Draw all the tiles in the game map
@@ -70,6 +70,14 @@ def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute,
 
     libtcod.console_set_default_background(panel, libtcod.black)
     libtcod.console_clear(panel)
+
+    # Print the game messages, one line at a time
+    y = 1
+    for message in message_log.messages:
+        libtcod.console_set_default_foreground(panel, message.color)
+        libtcod.console_print_ex(panel, message_log.x, y, libtcod.BKGND_NONE,
+                                 libtcod.LEFT, message.text)
+        y += 1
 
     render_bar(panel, 1, 1, bar_width, 'HP', player.fighter.hp,
                player.fighter.max_hp, libtcod.light_red, libtcod.darker_red)
