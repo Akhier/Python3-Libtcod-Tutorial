fcdeb0e6a77e04d28f6a912685de5abef407fee9
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-02-17 00:23:42 -0500

    Add get_names_under_mouse to  render_functions file

--- render_functions.py
+++ render_functions.py
@@ -9,6 +9,17 @@ class RenderOrder(Enum):
     ACTOR = 3
 
 
+def get_names_under_mouse(mouse, entities, fov_map):
+    (x, y) = (mouse.cx, mouse.cy)
+
+    names = [entity.name for entity in entities
+             if entity.x == x and entity.y == y and
+             libtcod.map_is_in_fov(fov_map, entity.x, entity.y)]
+    names = ', '.join(names)
+
+    return names.capitalize()
+
+
 def render_bar(panel, x, y, total_width, name, value,
                maximum, bar_color, back_color):
     bar_width = int(float(value) / maximum * total_width)
