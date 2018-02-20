ca9e18fd94c16123186c02c449fba0cf61b880f5
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-02-20 02:19:33 -0500

    Change place_entities to place items

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -100,9 +100,10 @@ class GameMap:
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
-    def place_entities(self, room, entities, max_monsters_per_room):
-        # get a random number of monsters
+    def place_entities(self, room, entities, max_monsters_per_room,
+                       max_items_per_room):
         number_of_monsters = randint(0, max_monsters_per_room)
+        number_of_items = randint(0, max_items_per_room)
 
         for i in range(number_of_monsters):
             # Choose a random location in the room
@@ -131,6 +132,17 @@ class GameMap:
                                      ai=ai_component)
 
                 entities.append(monster)
+
+        for i in range(number_of_items):
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities
+                        if entity.x == x and entity.y == y]):
+                item = Entity(x, y, '!', libtcod.violet, 'Healing Potion',
+                              render_order=RenderOrder.ITEM)
+
+                entities.append(item)
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
