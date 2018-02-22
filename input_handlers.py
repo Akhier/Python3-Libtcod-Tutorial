8a400bc0a3bc0c0271a38fe37ecb0e59cf4ae6e2
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-02-21 21:34:28 -0500

    Update hangle_keys to deal with GameStates.DROP_INVENTORY

--- input_handlers.py
+++ input_handlers.py
@@ -8,7 +8,7 @@ def handle_keys(key, game_state):
         return handle_player_turn_keys(key)
     elif game_state == GameStates.PLAYER_DEAD:
         return handle_player_dead_keys(key)
-    elif game_state == GameStates.SHOW_INVENTORY:
+    elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
         return handle_inventory_keys(key)
 
     return {}
