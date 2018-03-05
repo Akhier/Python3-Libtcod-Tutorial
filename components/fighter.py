f28f20e27b35f034bf80a7331d8ee86abb53b6e1
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-03-05 00:08:55 -0500

    Add xp variable to fighter

--- components/fighter.py
+++ components/fighter.py
@@ -4,11 +4,12 @@ from game_messages import Message
 
 
 class Fighter:
-    def __init__(self, hp, defense, power):
+    def __init__(self, hp, defense, power, xp=0):
         self.max_hp = hp
         self.hp = hp
         self.defense = defense
         self.power = power
+        self.xp = xp
 
     def take_damage(self, amount):
         results = []
