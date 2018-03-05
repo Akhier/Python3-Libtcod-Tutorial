cb9c6f4bbe5286549439aed547fa45142f7b3e0b
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-03-04 23:54:36 -0500

    Add placement of stairs to map gen

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -4,6 +4,7 @@ from random import randint
 from components.ai import BasicMonster
 from components.fighter import Fighter
 from components.item import Item
+from components.stairs import Stairs
 
 from entity import Entity
 from game_messages import Message
@@ -35,6 +36,9 @@ class GameMap:
         rooms = []
         num_rooms = 0
 
+        center_of_last_room_x = None
+        center_of_last_room_y = None
+
         for r in range(max_rooms):
             # random width and height
             w = randint(room_min_size, room_max_size)
@@ -60,6 +64,9 @@ class GameMap:
 
                 # center coordinates of new room, will be useful later
                 (new_x, new_y) = new_room.center()
+
+                center_of_last_room_x = new_x
+                center_of_last_room_y = new_y
 
                 if num_rooms == 0:
                     # this is the first room, where the player starts at
@@ -88,6 +95,12 @@ class GameMap:
                 # finally, append the new room to the list
                 rooms.append(new_room)
                 num_rooms += 1
+
+        stairs_component = Stairs(self.dungeon_level + 1)
+        down_stairs = Entity(
+            center_of_last_room_x, center_of_last_room_y, '>', libtcod.white,
+            'Stairs', render_order=RenderOrder.STAIRS, stairs=stairs_component)
+        entities.append(down_stairs)
 
     def create_room(self, room):
         # go through the tiles in the rectangle and make them passable
